5ac7625e056072c54ce23a64165ccb4d07203b29
theHotGod <anthony_paguio@dlsu.edu.ph>
2023-10-28 18:18:13 +0800

    fixed register activity. so far no buttons working

--- app/src/main/java/com/mobdeve/s16/paguio/anthony/sakuravn/HomeActivity.java
+++ app/src/main/java/com/mobdeve/s16/paguio/anthony/sakuravn/HomeActivity.java
@@ -2,13 +2,20 @@ package com.mobdeve.s16.paguio.anthony.sakuravn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
     }
 }
--- app/src/main/java/com/mobdeve/s16/paguio/anthony/sakuravn/RegisterActivity.java
+++ app/src/main/java/com/mobdeve/s16/paguio/anthony/sakuravn/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.mobdeve.s16.paguio.anthony.sakuravn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,6 +19,19 @@ public class RegisterActivity extends AppCompatActivity {
 
         tvLogin = findViewById(R.id.tvLogin);
         registerBtn = findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(v -> register(v));
+        tvLogin.setOnClickListener(v -> login(v));
     }
 
+    public void register(View v) {
+        Intent intent = new Intent(this, ChoosePlayerActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void login(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
