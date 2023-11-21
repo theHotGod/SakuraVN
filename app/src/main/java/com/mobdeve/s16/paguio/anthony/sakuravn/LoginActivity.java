821f0c73e96b17ca0ef9895d0cb5ea5c89e9f2fb
Mikey <pierre_espina@dlsu.edu.ph>
2023-11-21 21:42:26 +0800

    Added Firebase functionality and Writing into the DB functionality

--- app/src/main/java/com/mobdeve/s16/paguio/anthony/sakuravn/LoginActivity.java
+++ app/src/main/java/com/mobdeve/s16/paguio/anthony/sakuravn/LoginActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LoginActivity extends AppCompatActivity {
 
 
@@ -25,7 +28,6 @@ public class LoginActivity extends AppCompatActivity {
         userInput = findViewById(R.id.userInput);
         userPassword = findViewById(R.id.userPass);
         loginBtn = findViewById(R.id.loginBtn);
-
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/mobdeve/s16/paguio/anthony/sakuravn/models/UserAccount.java
+++ app/src/main/java/com/mobdeve/s16/paguio/anthony/sakuravn/models/UserAccount.java
@@ -2,7 +2,7 @@ package com.mobdeve.s16.paguio.anthony.sakuravn.models;
 
 public class UserAccount {
     // Attributes
-    private String userUID;
+    private String confirmPassword;
     private String email;
     private String username;
     private String password;
@@ -10,5 +10,54 @@ public class UserAccount {
 
     public enum Gender {
         M, F
+    }
+
+    public UserAccount() {
+    }
+
+    public UserAccount(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Gender getPlayerMC() {
+        return playerMC;
+    }
+
+    public void setPlayerMC(Gender playerMC) {
+        this.playerMC = playerMC;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
